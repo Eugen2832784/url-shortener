dbaca3743c00be3dae2a1395d82e5a50e3324b2a
EugeneDev <kovalonokjevgenij@gmail.com>
2026-05-03 06:11:58 +0300

    create SaveURL function and test it

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 	"os"
 	"url-shortener/internal/config"
+	"url-shortener/internal/storage/sqlite"
 )
 
 const (
@@ -16,13 +17,29 @@ func main() {
 	// TODO: implement config : cleanenv
 	cfg := config.MustLoad()
 
+	//TODO: init logger: slog
 	log := setupLogger(cfg.Env)
 	log.Info("starting url-shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
-	//TODO: init logger: slog
-
 	//TODO:init storage: sqlite
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to initialize storage", "error", err)
+		os.Exit(1)
+	}
+	id, err := storage.SaveURL("https://www.youtube.com/watch?v=rCJvW2xgnk0&t=2428s", "youtube")
+	if err != nil {
+		log.Error("failed to save URL", "error", err)
+		os.Exit(1)
+	}
+	log.Info("saved URL", "id", id)
+	id, err = storage.SaveURL("https://www.youtube.com/watch?v=rCJvW2xgnk0&t=2428s", "youtube")
+	if err != nil {
+		log.Error("failed to save URL", "error", err)
+		os.Exit(1)
+	}
 
+	_ = storage
 	//TODO:init router: chi, "chi render"
 
 	//TODO: run server:
---
+++ internal/storage/sqlite/sqlite.go
@@ -0,0 +1,56 @@
+package sqlite
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/mattn/go-sqlite3"
+)
+
+type Storage struct {
+	db *sql.DB
+}
+
+func New(StoragePath string) (*Storage, error) {
+	const op = "storage.sqlite.New"
+
+	db, err := sql.Open("sqlite3", StoragePath)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	stmt, err := db.Prepare(`
+CREATE TABLE IF NOT EXISTS url (
+    id INTEGER PRIMARY KEY,
+    alias TEXT not null UNIQUE,
+    url TEXT NOT NULL);
+CREATE INDEX IF NOT EXISTS idx_alias ON url (alias);
+    `)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	_, err = stmt.Exec()
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	return &Storage{db: db}, nil
+}
+func (s *Storage) SaveURL(urlToSave string, alias string) (int64, error) {
+	const op = "storage.sqlite.SaveURL"
+	stmt, err := s.db.Prepare("Insert into url (alias, url) values (?, ?)")
+	if err != nil {
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	res, err := stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && errors.Is(sqliteErr.ExtendedCode, sqlite3.ErrConstraintUnique) {
+			return 0, fmt.Errorf("%s: %w", op, err)
+		}
+
+	}
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, fmt.Errorf("%s:failed to get last insert id: %w", op, err)
+	}
+	return id, nil
+}
